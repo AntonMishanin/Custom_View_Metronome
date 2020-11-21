18a1b7672bc35c5d66f67cee627c36b3ac22b816
Anton Mishanin <antonmishanin@mail.ru>
2020-11-21 09:58:39 +0700

    Заготовка для метронома готова

--- app/src/main/java/com/example/customviewmetronome/MainActivity.kt
+++ app/src/main/java/com/example/customviewmetronome/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.customviewmetronome
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mMetronomeView: MetronomeView? = null
+    private var mBpmText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBpmText = findViewById<View>(R.id.bpm_text) as TextView
+        mMetronomeView = findViewById<View>(R.id.metronome_view) as MetronomeView
+        mMetronomeView?.bpm = 120f
+    }
+
+    fun setDegree(){
+
     }
 }
